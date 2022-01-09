286ac15f1be90c6e15b10e763370bcc3cefdb21e
Arya Krishna <dr.aryakris@outlook.com>
2022-01-08 23:27:21 -0800

    working with prompt, and creating questions in the terminal to ask the use

--- index.js
+++ index.js
@@ -2,67 +2,69 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateMarkdown = require('./utils/generateMarkdown.js');
-const { generateKey, generatePrime } = require('crypto');
-
-
-
-
+// const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [{
     type: 'input',
-    message: 'What is the project title?',
-    name: 'Title',
-
+    message: 'What is your GitHub user name?',
+    name: 'username'
 },
     {
         type: 'input',
-        message: 'What is your Description for the project?',
-        name: 'Description',
+        message: 'What is your email address?',
+        name: 'email'
     },
     {
         type: 'input',
-        message: 'What are the contents of the project?',
-        name: 'Contents',
+        message: 'What is your project title?',
+        name: 'title'
     },
     {
         type: 'input',
-        message: 'What are the Installation instructions?',
-        name: 'Installation',
+        message: 'Please write a short description of your project',
+        name: 'Description'
     },
     {
         type: 'input',
-        message: 'What are the usage information?',
-        name: 'Usage',
-
-
+        message: 'What kind of license does your project have?',
+        name: 'license'
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to install dependencies?',
+        name: 'dependencies'
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to run tests?',
+        name: 'tests'
+    },
+    {
+        type: 'input',
+        message: 'What does the user needs to know about contributing to tthe repo?',
+        name: 'contribution'
     },
 
 ];
-inquirer
-    .prompt([(questions)
-        /* Pass your questions in here */
-    ])
-    .then((answers) => {
-        // Use user feedback for... whatever!!
-        return generateMarkdown (answers);
 
-    })
-    .catch((error) => {
-        if (error.isTtyError) {
-            // Prompt couldn't be rendered in the current environment
-        } else {
-            // Something else went wrong
-        }
-    });
+// fs.writeFile('markDown.md', markDownContent, (err) =>
+//     err ? console.log(err) : console.log('Successfully created markDown.md!'));
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
+function writeFile(fileName, data) {
     module.exports;
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            console.log(answers); 
+        });
+ }
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,21 +1,48 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   console.log("Testing the terminal");
-  return `# ${data.title}
+  return `# ${data.Title}
+  ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+  ## Description
+  ${data.Description}
+
+  ## Table of Contents
+  *[Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+ 
+## Installation
+${data.Installation}
+
+## License
+This project is license under ${data.license}
+
+
+## Contributors 
+${data.contributors}
+
+
+## Tests
+${data.test}
+
+
 
 `;
 }
-
+// module.exports is an object we use to store variables or methods
 module.exports = generateMarkdown;
